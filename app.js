c658de2ad58da6da576907f8f31598eada9d10e9
Luis Edgar Llanos <dovahart@icloud.com>
2019-10-30 19:15:24 -0600

    CHECK uploads

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var uploadRouter = require('./routes/upload');
 var recordRouter = require('./routes/record');
+var videoRouter = require('./routes/video')
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/upload', uploadRouter);
 app.use('/record', recordRouter);
+app.use('/uploads', videoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/video.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var path = require('path');
+
+apt.get('/', function(req, res){
+	filepath = request.url;
+	filepath = path.resolve('.'+filepath);
+	fs.stat(filepath, function(err, stats){
+		if(err){
+			if(err.code === 'ENOENT'){
+				return res.sendStatus(404);
+			}
+			res.end(err);
+		}
+		var range = req.headers.range;
+		if(!range){
+			return res.sendStatus(416);
+		}
+		var positions = range.replace(/bytes=/, "").split("-");
+		var start = parseInt(positions[0],10);
+		var total = stats.size;
+		var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+		var chunksize = (end - start) + 1;
+
+		res.writeHead(206, {
+			"Content-Range": "bytes" + start + "-" + end + "/" + total,
+			"Accept=ranges": "bytes",
+			"Content-Length": chunksize,
+			"Content-Type": "video/mp4"
+		});
+
+		var stream = fs.createReadStream(filepath, {start: start, end: end})
+		.on("open", function(){
+			stream.pipe(res);
+		})
+		.on("error", function(err){
+			res.end(err);
+		});
+	});
+});
