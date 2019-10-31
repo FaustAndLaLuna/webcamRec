446e90e60d31b61236b4ab93e80a38a8450df608
Luis Edgar Llanos <dovahart@icloud.com>
2019-10-31 12:43:40 -0600

    Update video.js

--- routes/video.js
+++ routes/video.js
@@ -6,10 +6,10 @@ var router = express.Router();
 router.get("/:first/:second/:third/:fourth/:filename", function(req, res){
 	filename = "/" + req.params.first + "/" + req.params.second + "/" + req.params.third + "/" + req.params.fourth + "/" + req.params.filename;
 	filepath = path.resolve('./uploads' + filename);
-	src = fs.createReadStream(filepath);
-	src.pipe(res);
+	//src = fs.createReadStream(filepath);
+	//src.pipe(res);
 	//res.sendFile(filepath);
-	/*fs.stat(filepath, function(err, stats){
+	fs.stat(filepath, function(err, stats){
 		if(err){
 			if(err.code === 'ENOENT'){
 				return res.sendStatus(404);
