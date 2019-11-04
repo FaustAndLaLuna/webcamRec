064160a536b1a147572d926c81a805a86d2a979e
Luis Edgar Llanos <dovahart@icloud.com>
2019-11-03 22:40:06 -0600

    Metadata Try

--- routes/upload.js
+++ routes/upload.js
@@ -14,7 +14,7 @@ const dao = new AppDAO('./database.sqlite3');
 const vidTable = new videosRepo(dao);
 const genThumbnail = require('simple-thumbnail');
 
-const SIZE = '640x?';
+const SIZE = '480x?';
 
 
 vidTable.createTable();
@@ -65,11 +65,13 @@ router.post('/', function(req, res, next){
 					.videoCodec('libx264')
 					.on('end', () =>{
 						vidTable.create(filename+".mp4", Date.now().toString());
+						/*
 						mkdirp(path.dirname(filePath.replace("uploads", "public/thumbs")), (err) => {
 							if (err)
 								console.log(err);
 							genThumbnail(convFilePath, convFilePath.replace("mp4", "png").replace("uploads", "public/thumbs"), SIZE);
 						});
+						*/	
 						console.log("uploaded and converted to: " + filename+".mp4");
 						fs.unlink(filePath, (err) => {
 							if(err){
