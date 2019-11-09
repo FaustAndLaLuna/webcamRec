84b1760328fa59b35544fb6c375a33d72460c0ee
Luis Edgar Llanos <dovahart@icloud.com>
2019-11-08 21:43:12 -0600

    Update vidPlayer.js

--- routes/vidPlayer.js
+++ routes/vidPlayer.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 router.get('/:first/:second/:third/:fourth/:filename', (req, res) => {
 	resObj = {}
-	resObj.videoURL = '/' + first + '/' + second + '/' + third +
-					"/" + fourth + '/' + filename;
+	resObj.videoURL = '/' + req.params.first + '/' + req.params.second + '/' + req.params.third +
+					"/" + req.params.fourth + '/' + req.params.filename;
 	resObj.title = 'Video';
 	res.render('vid', resObj);
 });
