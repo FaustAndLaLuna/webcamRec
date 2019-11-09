74912ad43eeeb57bac8e6c71754e224c456e78f8
Luis Edgar Llanos <dovahart@icloud.com>
2019-11-08 20:17:40 -0600

    Thumb

--- routes/upload.js
+++ routes/upload.js
@@ -65,13 +65,11 @@ router.post('/', function(req, res, next){
 					.videoCodec('libx264')
 					.on('end', () =>{
 						vidTable.create(filename+".mp4", Date.now().toString());
-						/*
 						mkdirp(path.dirname(filePath.replace("uploads", "public/thumbs")), (err) => {
 							if (err)
 								console.log(err);
 							genThumbnail(convFilePath, convFilePath.replace("mp4", "png").replace("uploads", "public/thumbs"), SIZE);
 						});
-						*/	
 						console.log("uploaded and converted to: " + filename+".mp4");
 						fs.unlink(filePath, (err) => {
 							if(err){
