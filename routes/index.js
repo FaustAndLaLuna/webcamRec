192ae3c17d6dea9b48c969527f4e5b807df618f0
Luis Edgar Llanos <dovahart@icloud.com>
2019-12-12 19:38:54 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -13,16 +13,18 @@ router.get('/', function(req, res, next) {
 	resObj = {};
 	resObj.title = "Todos los videos";
 	
-	vidTable.getAll().then((allVids) => {
-		resObj.allVids = allVids;
-		res.render('index', resObj);
-	});
-	
 	if(!ISENCODING){
 		vidTable.getNextEncodable().then((nextEncodableVideo) => {
 		console.log(nextEncodableVideo.tempURL);
 		encodeMod.encode(nextEncodableVideo.tempURL);});
 	}
+	
+	vidTable.getAll().then((allVids) => {
+		resObj.allVids = allVids;
+		res.render('index', resObj);
+	});
+	
+	
 	
 	//res.sendFile(path.resolve('./public/record.html'));
 });
