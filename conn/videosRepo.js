a93e252e4ec3ce40f9558796a53a05ec411d2ba1
Luis Edgar Llanos <dovahart@icloud.com>
2019-12-30 22:31:58 -0600

    Update videosRepo.js

--- conn/videosRepo.js
+++ conn/videosRepo.js
@@ -5,15 +5,12 @@ class videosRepo{
 	constructor(){
 		
 		var pw = fs.readFileSync('./password.p', 'utf8');
-		console.log(pw.slice(0,12));
 		pw = pw.slice(0,12);
 		var conn = mysql.createConnection({
 		host: 'localhost',
 		user: 'root',
-		password: pw,
-		database: 'biografo'
+		password: pw
 		});
-		console.log(pw);
 		conn.connect(function(err){
 			if(err) console.log(err);
 			console.log("Connected to videos MySQL table!")
