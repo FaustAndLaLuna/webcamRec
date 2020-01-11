6e34ca52d736f6ec2e55d8c096abe9cfead99d14
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-11 11:28:34 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,8 @@ var vidPlayerRouter = require('./vidPlayer.js');
 
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated())
-		return next();
-	res.redirect('/login');
+		next();
+	else res.redirect('/login');
 }
 
 module.exports = function(app, passport){
