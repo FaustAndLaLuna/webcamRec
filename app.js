732cc89e78125aa21fd662db207e8148920e2393
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-11 09:22:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -51,7 +51,10 @@ app.use(session({
 	cookie: {secure: true, httpOnly: false, path: '/', maxAge: 259200000}
 }));
 
-
+app.use(function(req,res,next){
+	if(!req.secure )
+		return res.redirect('https://' + req.get('host') + req.url);
+});
 
 require('./routes/routes.js')(app, passport);
 //IMPORTANT LINE;
