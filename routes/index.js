fc43519e03d81d177e9f02e18c4b3de89e603551
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-26 19:54:05 -0600

    asdf

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@ router.get('/', function(req, res, next) {
 	
 	objectsTable.getRandomLimited(5).then(function(objects){
 		req.responseObj.objects = objects;
-		console.log(objects);
 		res.render('index.ejs', req.responseObj);
 	});
 });
