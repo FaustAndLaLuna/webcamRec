72f7c7561de78e809e00c57efb6fed5ad08dca63
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-27 06:01:39 -0600

    Update sell.js

--- routes/sell.js
+++ routes/sell.js
@@ -12,52 +12,59 @@ router.get('/', function(req,res,next){
 	res.render('vender.ejs', req.responseObj);
 	});
 
-router.post('/', function(req,res,next){
-	var filename = uuidv4();
-	var thumbURL = "/"+ filename.slice(0,1)+"/"+filename.slice(1,2)+"/"+filename.slice(2,3)+
-				"/"+filename.slice(3,4)+"/" + filename.slice(4) + ".";
-	var filePath = path.resolve('./thumbs'+filename+".webm");
-	mkdirp(path.dirname(filePath), function(err){
-		if(err) console.log(err);
-		var form = new formidable.IncomingForm();
-		form.on('fileBegin', function (name, file){
-			var fTypeCheck = file.type;
-			if(fTypeCheck.match("^image/")){
-				filePath += fTypeCheck.replace("image/", "");
-			}
-			file.path = filePath;
-			console.log(file);
-		}.bind({filePath:filePath}));
-		
-		form.on('error', function(err){
-			console.log('An error has occurred uploading a file:\n ' + err);
-		});
-		
-		form.parse(req, function(err, fields, files){
-			console.log(files);
-			/* var photoArray = [];
-			files.forEach(function(file){
-				var fTypeCheck = file.type;
-				if(!fTypeCheck.match("^image/")){
-					fs.unlink(file.path, function(err){
-						if(err){
-							console.log(err);
-						}
-					});
-				}
-				else{
-					photoArray.push();
-				}
-			});
-			photoArrayString = JSON.stringify(photoArray);
-			
-			objectsDB.create(fields.name, fields.offeringUserID, fields.isAuction == "true"? true: false, fields.description, fields.story, fields.endDate, photoArrayString);
-			res.write("Objeto puesto en venta exitosamente!");
-			res.end();		 */
-					
-			}.bind({filePath:filePath}));
-		
-	}.bind({filePath:filePath}));
+router.post('/', function(req, res, next){
+	//TODO: Add userID and objectID.
+	filename = uuidv4();
+	thumbFolder = "/"+ filename.slice(0,1)+"/"+filename.slice(1,2)+"/"+filename.slice(2,3)+
+				"/"+filename.slice(3,4)+"/";
+	thumbName = filename.slice(4) + ".";
+	filename = thumbFolder + filename.slice(4);
+	filePath = path.resolve('./thumbs'+filename+".");
+	convFilePath = path.resolve('./thumbs'+filename+".");
+	mkdirp(path.dirname(filePath), function (err){
+		if(err)
+			console.log(err);
+		mkdirp(path.dirname(filePath.replace("uploads", "public/thumbs")), function (err){
+			if (err)
+				console.log(err);
+
+			fs.writeFile(filePath, '', function (err){
+				if(err)  console.log(err);
+				fTypeCheck = "";
+
+				var form = new formidable.IncomingForm();
+				form.on('fileBegin', function (name, file){
+					file.path = filePath;
+					fTypeCheck = file.type;
+				}.bind({filename:filename, thumbFolder:thumbFolder, filePath:filePath, convFilePath:convFilePath}));
+				form.on('error', function(err){
+					console.log('An error has occurred uploading a file:\n ' + err);
+				});
+				form.parse(req, function(err, fields, files){
+					console.log(fTypeCheck);
+					if(!fTypeCheck.match("^video/")){
+						fs.unlink(filePath, function(err){
+							if(err){
+								console.log(err);
+							}
+						});
+						res.write("<h2>Tipo de archivo incorrecto!</h2> <br> <h1>Intenta subir un video</h1>");
+						res.end();
+						return;
+					}
+					let filePath = this.filePath;
+					vidTable.createAssociated("SIN URL", filePath, fields.user.id, fields.obj.objectID, fields.description, fields.title, fields.tags);
+					res.write("Video subido exitosamente!");
+					res.end();
+				}.bind({filename:filename, thumbFolder:thumbFolder, filePath:filePath, convFilePath:convFilePath}));
+			}.bind({filename:filename, thumbFolder:thumbFolder, filePath:filePath, convFilePath:convFilePath}) );
+		}.bind({filename:filename, thumbFolder:thumbFolder, filePath:filePath, convFilePath:convFilePath}) );	
+	}.bind({filename:filename, thumbFolder:thumbFolder, filePath:filePath, convFilePath:convFilePath}) );
 });
+
+
+
+
+
 
 module.exports = router;
