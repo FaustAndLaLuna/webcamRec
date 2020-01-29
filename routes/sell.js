704cab47864567b986744f4be318b031b113d12a
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-29 17:22:49 -0600

    Update sell.js

--- routes/sell.js
+++ routes/sell.js
@@ -17,7 +17,7 @@ router.post('/', function(req, res, next){
 	filePath = uuidv4();
 	filePath = "/" + filePath.slice(0,1) + "/" + filePath.slice(1,2) + "/" + filePath.slice(2,3) + "/" + filePath.slice(3,4) + "/";
 	thumbFilePath = "/public/objects/thumbs" + filePath;
-	mkdirp.sync(thumbFilePath);
+	mkdirp.sync(path.dirname(thumbFilePath));
 	formObject = {uploadDir: thumbFilePath, keepExtensions: true};
 	var form = new formidable.IncomingForm(formObject);
 	form.on('error', function(err){
