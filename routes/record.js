1cab854bcf1fa2489fa4e5b706707661ee1520ec
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-29 19:44:32 -0600

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -2,14 +2,24 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
+const objectsRepo = require('../conn/objectsRepo.js');
+var createError = require('http-errors');
+const objectsDB = new objectsRepo();
+
 // TODO Set Index
 router.get('/', function(req, res, next) {
-	userAgent = req.get('User-Agent');
-	if(!!userAgent.match("/iPad|iPhone|iPod/")){
-		res.render('recordiOS.ejs', req.responseObj);
-	} else {
-		res.render('record.ejs', req.responseObj);
-	}
+	objectsDB.getObject(req.query.objectID).then(
+		function(obj){
+			req.responseObj.obj = obj;
+			userAgent = req.get('User-Agent');
+			if(!!userAgent.match("/iPad|iPhone|iPod/")){
+				res.render('recordiOS.ejs', req.responseObj);
+			} else {
+				res.render('record.ejs', req.responseObj);
+			}
+		}
+	);
+	
 });
 
 
