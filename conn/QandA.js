a646251cb4664f105f5f9c190bdb05e1b4e0ebe4
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-29 18:35:43 -0600

    asdf

--- conn/QandA.js
+++ conn/QandA.js
@@ -3,11 +3,16 @@ var POOL = require('./pool').POOL;
 
 class questionsRepo{
 	constructor(){
-		const sql = `CREATE TABLE IF NOT EXISTS contactMessages(
-			messageID int PRIMARY KEY AUTO_INCREMENT,
-			message TEXT,
-			name TINYTEXT,
-			email TINYTEXT);`
+		const sql = `CREATE TABLE IF NOT EXISTS questions(
+			questionID int PRIMARY KEY AUTO_INCREMENT,
+			question TEXT,
+			answer TEXT,
+			objectID int,
+			CONSTRAINT fk_object
+			FOREIGN KEY (objectID)
+			REFERENCES objects(objectID)
+				ON UPDATE CASCADE
+				ON DELETE CASCADE);`
 				
 		POOL.getConnection(function (error, conn){
 			conn.query(sql, function(err, result){
@@ -15,14 +20,25 @@ class questionsRepo{
 				conn.release();
 			});
 		});
-		console.log("BIOGRAFO.contactMessages created");
+		console.log("BIOGRAFO.questions created");
 	}
 	
-	create(message, name, email){
-		let q = "INSERT INTO questions (message, name, email) VALUES (?, ?, ?);";
+	create(question, objectID){
+		let q = "INSERT INTO questions (question, objectID) VALUES ( ?, ?);";
 		POOL.getConnection(function (err, conn){
-			conn.query(q, [message, name, email], function(err, result){
+			conn.query(q, [question, objectID], function(err, result){
 				if (err)	console.log(err);
+				conn.release();
+				return;
+			});
+		});
+	}
+	
+	answer(questionID, answer){
+		let q = "UPDATE questions SET answer = ? WHERE questionID = ?;"
+		POOL.getConnection(function(err, conn){
+			conn.query(q, [answer, questionID], function(err, result){
+				if(err) console.log(err);
 				conn.release();
 				return;
 			});
@@ -43,7 +59,7 @@ class questionsRepo{
 			});	
 		}
 	
-	getAllQuestionsFromUser(questionUserId){
+/* 	getAllQuestionsFromUser(questionUserId){
 		let q = "SELECT * FROM questions WHERE userID = ?;";
 		return new Promise(function(resolve, reject){
 			POOL.getConnection(function (err, conn){
@@ -69,7 +85,7 @@ class questionsRepo{
 				});
 			});
 		});	
-	}
+	} */
 }
 
 
