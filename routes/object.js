38c701c80e39aece47135094d86755ea50ef5036
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-29 18:41:25 -0600

    Update object.js

--- routes/object.js
+++ routes/object.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var path = require('path');
 const objectsRepo = require('../conn/objectsRepo.js')
 const QandARepo = require('../conn/QandA.js');
+var createError = require('http-errors');
 const questionsDB = new QandARepo();
 const objectsDB = new objectsRepo();
 
