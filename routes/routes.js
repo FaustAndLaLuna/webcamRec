65a31c6fcf35051069f2419fb26a130cbbf1eb6e
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-30 08:16:17 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,12 +6,16 @@ var videoRouter 	= require('./video.js');
 var vidPlayerRouter = require('./vidPlayer.js');
 var contactPostRouter = require('./contacto.js');
 var objectRouter = require('./object.js');
+var recuerdosRouter = require('./recuerdos.js');
 var sell = require('./sell.js');
 
 module.exports = function(app, passport){
 	
 	app.use("/", indexRouter);
-	app.use("/index.html", indexRouter);
+	app.use("/index.html", function(req,res,next){
+		res.redirect('/');
+	});
+	app.use("/recuerdos.html", recuerdosRouter);
 	app.get('/aboutUs.html', function(req, res, next) {
 		res.render('aboutUs.ejs', req.responseObj);
 	});
