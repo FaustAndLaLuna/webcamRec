a4da39d163277d637af725ffc613c8695b7b0c2f
Luis Edgar Llanos <dovahart@icloud.com>
2020-01-30 05:23:49 -0600

    Update objectsRepo.js

--- conn/objectsRepo.js
+++ conn/objectsRepo.js
@@ -15,7 +15,7 @@ class objectsRepo{
 			endDate datetime NOT NULL,
 			createdAt datetime NOT NULL,
 			images varchar(1000),
-			CONSTRAINT fk_user
+			CONSTRAINT fk_userID
 			FOREIGN KEY (offeringUserID)
 			REFERENCES users(id)
 				ON UPDATE CASCADE
