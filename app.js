3f0dc426ace86b6c4fa71150485b177fd0cd6a64
Luis Edgar Llanos <dovahart@icloud.com>
2020-02-17 14:26:12 -0600

    Update app.js

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use(passport.session());
 
 app.use(function(req,res,next){
 	cookies = req.cookies;
+	console.log(cookies);
 	if(cookies.hasOwnProperty("redirect")){
 		redirectCookie = cookies[redirect];
 		redirectCookie.lastAddress = redirectCookie.currAddress;
