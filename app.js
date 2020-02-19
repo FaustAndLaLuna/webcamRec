df438a61bd9cdfbfa79eecd52c4b80e80f7c540e
Luis Edgar Llanos <dovahart@icloud.com>
2020-02-19 13:38:38 -0600

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.use(passport.session());
 app.use(function(req,res,next){
 	cookies = req.cookies;
 	console.log(cookies);
-	cookies = (typeof cookies === "undefined" || cookies == null) ? {} : cookies;
+	cookies = (typeof cookies === "undefined" || cookies == null) ? console.log("isNull or undefined") : cookies;
 	console.log(cookies);
 	if(cookies.hasOwnProperty("redirect")){
 		redirectCookie = cookies.redirect;
