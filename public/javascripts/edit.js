b167cf03174d89c6333d2589201df3af8e26bd5b
fall <dovahart@icloud.com>
2022-11-25 17:45:24 -0600

    asdf

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -26,3 +26,50 @@ function wordsNotInDict(phrase){
 	}
 	return wordsNotFound;
 }
+function getPhraseVideoObject(phrase){
+	phrase = phrase.toLowerCase();
+	splitPhrase = phrase.split(' ');
+	wordsNotFound = wordsNotInDict(phrase);
+	ans = [];
+	if(wordsNotFound.length > 0){
+		console.log('No se puede generar esta frase, faltan las palabras:');
+		console.log(wordsNotFound);
+		return;
+	} else {
+		for(let i = 0; i < splitPhrase.length; i++){
+			let choose = Math.floor(Math.random() * wordDict[splitPhrase[i]].length);
+			ans.push(wordDict[splitPhrase[i]][choose]);
+		}
+		return ans;
+	}
+}
+
+function destroyVideoElement(element){
+	element.remove();
+}
+
+function createVideoElement(source){
+	let startTime = source.startTime;
+	let delta = ((source.endTime - source.startTime) * 1000);
+	let video = document.createElement('video');
+	video.src = 'uploads'+source.videoURL;
+	video.controls = false;
+	video.muted = false;
+	video.addEventListener('loadedmetadata', () => {this.currentTime = startTime});
+	let container = document.getElementById('videoContainer');
+	container.appendChild(video);
+	setTimeout(destroyVideoElement, delta, video);
+}
+
+function createVideoSequence(videoObj){
+	let delta = 0;
+	for(let i = 0; i < videoObj.length; i++){
+		setTimeout(createVideoElement, delta, videoObj[i]);
+		delta += ((videoObj[i].endTime - videoObj[i].startTime) * 1000);
+	}
+}
+
+function createEditFromPhrase(phrase){
+	videoObject = getPhraseVideoObject(phrase);
+	createVideoSequence(videoObject);
+}
