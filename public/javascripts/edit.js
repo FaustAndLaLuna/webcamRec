bc91812019ff373e6979b3ce868d9c5a027f24bc
Luis Edgar Llanos <edgardl@exatec.tec.mx>
2023-01-02 14:52:59 -0600

    Update edit.js

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -4,6 +4,7 @@ stopwords = ['a','actualmente','adelante','además','afirmó','agregó','ahora',
 jQuery.getJSON('/edicion', (data) => {
 	for(let i = 0; i < data.length; i++){
 		for(let j = 0; j < data[i].transcription.length; j++){
+			data[i].transcription[j].word = data[i].transcription[j].word.replaceAll('"','');
 			toAdd = {	videoURL: data[i].videoURL,
 						endTime: data[i].transcription[j].endTime,
 						startTime: data[i].transcription[j].startTime,
@@ -18,36 +19,12 @@ jQuery.getJSON('/edicion', (data) => {
 	for(let i = 0; i < stopwords.length; i++){
 		delete wordDict[stopwords[i]];
 	};
+	for(word in wordDict){
+		if(wordDict[word].length < 4){
+			delete wordDict[word];
+		}
+	} 
 })
-function wordsNotInDict(phrase){
-	phrase = phrase.toLowerCase();
-	splitPhrase = phrase.split(' ');
-	wordsNotFound = [];
-	for(let i = 0; i < splitPhrase.length; i++){
-		if(splitPhrase[i] in wordDict){
-		} else {
-			wordsNotFound.push(splitPhrase[i])
-		}
-	}
-	return wordsNotFound;
-}
-function getPhraseVideoObject(phrase){
-	phrase = phrase.toLowerCase();
-	splitPhrase = phrase.split(' ');
-	wordsNotFound = wordsNotInDict(phrase);
-	ans = [];
-	if(wordsNotFound.length > 0){
-		console.log('No se puede generar esta frase, faltan las palabras:');
-		console.log(wordsNotFound);
-		return;
-	} else {
-		for(let i = 0; i < splitPhrase.length; i++){
-			let choose = Math.floor(Math.random() * wordDict[splitPhrase[i]].length);
-			ans.push(wordDict[splitPhrase[i]][choose]);
-		}
-		return ans;
-	}
-}
 
 function destroyVideoElement(element){
 	element.pause();
@@ -70,17 +47,4 @@ function createVideoElement(source){
 	  }, false);
 	let container = document.getElementById('videoContainer');
 	container.appendChild(video);
-}
-
-function createVideoSequence(videoObj){
-	let delta = 0;
-	for(let i = 0; i < videoObj.length; i++){
-		setTimeout(createVideoElement, delta, videoObj[i]);
-		delta += ((videoObj[i].endTime - videoObj[i].startTime) * 1000)+100;
-	}
-}
-
-function createEditFromPhrase(phrase){
-	videoObject = getPhraseVideoObject(phrase);
-	createVideoSequence(videoObject);
 }
